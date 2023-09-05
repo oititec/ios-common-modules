832cc3bd4e4ef2503d25dc9ad80195bcf9f6322c
Vitor Souza <vitorx1280@gmail.com>
2023-09-05 18:11:04 -0300

    feat: add common modules

---
+++ Frameworks/OICommons.xcframework/ios-arm64_x86_64-simulator/OICommons.framework/Headers/OICommons.h
@@ -0,0 +1,15 @@
+//
+//  OICommons.h
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OICommons.
+FOUNDATION_EXPORT double OICommonsVersionNumber;
+
+//! Project version string for OICommons.
+FOUNDATION_EXPORT const unsigned char OICommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OICommons/PublicHeader.h>
+
+
